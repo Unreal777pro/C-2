9379c5a4184ae6a025668551d33b3e29ee5fe1c9
Unreal <unreal777x@gmail.com>
2022-07-30 00:09:35 +0300

    Sum

---
+++ Work012/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int tempNumber = number;
+int sum = 0;
+
+
+while (tempNumber > 1)
+{
+    sum+=tempNumber%10;
+    tempNumber/=10;    
+}
+
+Console.WriteLine(number +"->" + sum);
