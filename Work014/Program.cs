5470dc468520f09ad8be570661ac50c959f5320c
Unreal <unreal777x@gmail.com>
2022-07-30 00:28:07 +0300

    Array2

---
+++ Work014/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int size = InputInt("Задайте размер массива: ");
+int[] positives = FillArrayRandomly(size, 100,1000);
+
+PrintArray(positives);
+Console.WriteLine("Количество четных чисел равно " + CountEvens(positives));
+
+int CountEvens(int[] array)
+{
+  int evens = 0;
+
+  for(int i = 0; i < array.Length; i++)
+  {
+    if (array[i] % 2 == 0)
+       evens++;
+  }
+  
+  return evens;
+}
+
+int[] FillArrayRandomly(int numberOfElements, int minValue = 0, int maxValue = 10)
+{
+    int[] array = new int[numberOfElements]; 
+    for (i = 0; i < numberOfElements; i++)
+    {
+        array[i] = new Random().Next(minValue,maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    
+    
+    Console.WriteLine();   
+    
+    for (i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    
+    Console.WriteLine();   
+}
+
+int InputInt(string output)
+{
+  Console.WriteLine(output);
+  int number = Convert.ToInt32(Console.ReadLine());
+  return number;
+}
