eb482601835d32f8981d9518431a5ade979a2fb6
Unreal <unreal777x@gmail.com>
2022-07-30 00:05:11 +0300

    NatAB

---
+++ Work011/Program.cs
@@ -0,0 +1,28 @@
+﻿//программа принимает на вход числа А и В и возводит А в натуральную степень В
+
+
+int number = InputInt("Введите число: ");
+int powInt = InputInt("Введите степень в виде натурального числа: ");
+
+if (powInt < 0) Console.WriteLine("Степень должна быть натуральным числом");
+else Console.WriteLine($"{number},{powInt} -> {Pow(number,powInt)}");
+
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+double Pow(double number, int powNumber)
+{
+   int count = 1;
+   double pow = 1;    
+   while (count <= powNumber)
+   {
+       pow*=number;
+       count++;
+   }
+   return pow;
+}
+
