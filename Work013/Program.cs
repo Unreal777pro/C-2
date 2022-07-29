1b30ad4039fe391297f996a81731644e57b3bbf6
Unreal <unreal777x@gmail.com>
2022-07-30 00:13:51 +0300

    Array

---
+++ Work013/Program.cs
@@ -0,0 +1,34 @@
+﻿//программа задает массив из 8 элементов и выводит их на экран
+
+
+
+int [] randomArray = FillArrayRandomly(8,-100,100);
+PrintArray(randomArray);
+
+
+int[] FillArrayRandomly(int numberOfElements, int minValue = 0, int maxValue = 10)
+{
+    int[] array = new int[numberOfElements]; 
+    int i =0;
+    for (i = 0; i < numberOfElements; i++)
+    {
+        array[i] = new Random().Next(minValue,maxValue + 1);
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    int i = 0;
+    
+    
+    Console.WriteLine();   
+    
+    for (i= 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    
+    Console.WriteLine();   
+}
