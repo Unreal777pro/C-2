0438a15f3b3d03fbd104c1c5abda193d3a416120
Unreal <unreal777x@gmail.com>
2022-07-29 23:42:08 +0300

    Cubes

---
+++ Work010/Program.cs
@@ -0,0 +1,14 @@
+﻿//программа принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+string sequenceNCube = String.Empty;
+
+while (count <= number) 
+{
+    sequenceNCube += count*count*count + " ";
+    count++;
+}
+
+Console.WriteLine(number + "->" + sequenceNCube);
