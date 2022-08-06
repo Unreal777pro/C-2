166e9b245d47e79805ace1a4aa80a8f7cc3459ad
Unreal <unreal777x@gmail.com>
2022-08-06 09:06:03 +0300

    mxn

---
+++ Work019/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. 
+
+int rowsNumber = InputInt("Введите количество строк: ");
+int columnsNumber = InputInt("Введите количество столбцов: ");
+double[,] matrix = new double[rowsNumber, columnsNumber];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
+
+void FillMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next() + new Random().NextDouble();
+    }
+}
+
+void PrintMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
