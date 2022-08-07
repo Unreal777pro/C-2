7f7b058cc95a6948dd5d152139449da66eaa14ea
Unreal <unreal777x@gmail.com>
2022-08-07 09:42:37 +0300

    SpiralArray

---
+++ Work026/Program.cs
@@ -0,0 +1,52 @@
+﻿//Заполните спирально массив 4 на 4.
+
+int spiralSize = 4;
+int[,] spiralArray = new int[spiralSize, spiralSize];
+
+FillArraySpirally(spiralArray);
+PrintMatrix(spiralArray);
+
+void FillArraySpirally(int[,] array)
+{
+    int row = 0;
+    int column = 0;
+    int rowDirection = 1;
+    int columnDirection = 0;
+    int changeDirection = 0;
+    int touches = array.GetLength(0);
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        array[row, column] = i + 1;
+        if (--touches == 0)
+        {
+            touches = array.GetLength(0) * (changeDirection % 2) + array.GetLength(1) * ((changeDirection + 1) % 2) - (changeDirection / 2 - 1) - 2;
+            int temp = rowDirection;
+            rowDirection = -columnDirection;
+            columnDirection = temp;
+            changeDirection++;
+        }
+
+        column += rowDirection;
+        row += columnDirection;
+
+    }
+}
+
+
+
+
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
