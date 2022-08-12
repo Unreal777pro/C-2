39453e95f0a653fc8f1210ff07f5f60f0ec622d9
Unreal <unreal777x@gmail.com>
2022-08-12 21:39:48 +0300

    Akkerman

---
+++ Work029/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Введите M: ");
+int m = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine($"Значение функции Аккермана составляет {Akkerman(m, n)}");
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else if (m > 0 && n > 0)
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    else
+        return 1;
+}
